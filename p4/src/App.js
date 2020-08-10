1e9aec41fd27f00459ae442c012079fae9209667
Lunacy Rising <rerirani20@gmail.com>
2020-08-10 09:56:22 -0300

    otro titulo mas

--- p4/src/App.js
+++ p4/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>HOLA4</h1>
         <h1>HOLA5</h1>
+        <h1>HOLA6</h1>
       </header>
     </div>
   );
